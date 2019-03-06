bf06ee6f93a4f8345c64ff36243ee5b8279e0291
mliebich <michi.liebich@gmail.com>
2019-03-06 21:39:27 +0100

    neue zelle

--- kshp.py
+++ kshp.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 from turtle import *
 
+
 def makeTurtle():
     turtle = Turtle()
+
 
 def hideTurtle():
     hideturtle()
 
+
 def setPenColor(color):
     pencolor(color)
+
 
 def isInteger(n):
     print('isInteger-Aufruf')
